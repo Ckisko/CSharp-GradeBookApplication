f38f2fa3e2033c73a4187a487844e9a5f275382f
Ckisko <48059859+Ckisko@users.noreply.github.com>
2019-03-21 07:02:27 -0700

    Updating the gradebook
    Finishing the first set of tasks

---
+++ GradeBook/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+using GradeBook.GradeBooks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+using GradeBook.GradeBooks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    internal class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
