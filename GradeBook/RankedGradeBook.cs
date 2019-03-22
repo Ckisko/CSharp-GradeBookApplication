34032b28eada4d5102840f3a2f9abac475df2a7e
Ckisko <48059859+Ckisko@users.noreply.github.com>
2019-03-21 20:26:35 -0700

    Update RankedGradeBook.cs
    Adding the ability to Ranked-grade by finding the thresholds of each grade type based on the total amount of grades and returning the letter grade.

--- GradeBook/RankedGradeBook.cs
+++ GradeBook/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using GradeBook.GradeBooks;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook
@@ -11,6 +12,41 @@ namespace GradeBook
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidCastException("Ranked-grading requires a minimum of 5 students to work");
+                
+            }
+
+            var threshold = (int)Math.Ceiling (20 / 100 * Students.Count);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+
+            }
+            else
+            {
+                return 'F';
+            }
+            
         }
     }
 }
